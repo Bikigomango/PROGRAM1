5b44b18ca04e5027e5aa06f7e19a12d1f358914d
Bikigomango <129982236+Bikigomango@users.noreply.github.com>
2023-04-13 13:40:55 +0530

    Create program1.java
    if else statement

---
+++ program1.java
@@ -0,0 +1,13 @@
+public class Main{
+      public static void main(String[] args){
+            int a = 10;
+            int b =20;
+            if(a==b){
+                  System.out.println("it is a square");
+            else{
+                  System.out.println("it is a rectangle");
+            }
+            
+          }
+      }
+}
